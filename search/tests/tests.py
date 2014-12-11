38d87d3f5e06635920d0d60f8f467ce566be6ef4
Martyn James <mjames@edx.org>
2014-12-11 15:34:23 -0500

    Moved files from common prototype

---
+++ search/tests/tests.py
@@ -0,0 +1,380 @@
+from django.test import TestCase
+from django.test.utils import override_settings
+from elasticsearch import Elasticsearch
+# from nose.tools import set_trace
+
+from search.manager import SearchEngine
+from search.elastic import ElasticSearchEngine
+
+from .mock_search_engine import MockSearchEngine
+from search.views import SearchResultProcessor
+
+TEST_INDEX_NAME = "test_index"
+
+# We override ElasticSearchEngine class in order to force an index refresh upon index
+# otherwise we often get results from the prior state, rendering the tests less useful
+
+
+class ForceRefreshElasticSearchEngine(ElasticSearchEngine):
+
+    def index(self, doc_type, body, **kwargs):
+        kwargs.update({
+            "refresh": True
+        })
+        super(ForceRefreshElasticSearchEngine, self).index(doc_type, body, **kwargs)
+
+TEST_ENGINE = MockSearchEngine
+# Uncomment below in order to test against Elastic Search installation
+# TEST_ENGINE = ForceRefreshElasticSearchEngine
+
+
+@override_settings(SEARCH_ENGINE=TEST_ENGINE)
+class ElasticSearchTests(TestCase):
+
+    _searcher = None
+
+    @property
+    def searcher(self):
+        if self._searcher is None:
+            self._searcher = SearchEngine.get_search_engine(TEST_INDEX_NAME)
+        return self._searcher
+
+    @property
+    def _is_elastic(self):
+        return isinstance(self.searcher, ElasticSearchEngine)
+
+    def setUp(self):
+        if self._is_elastic:
+            es = Elasticsearch()
+            # Make sure that we are fresh
+            es.indices.delete(index=TEST_INDEX_NAME, ignore=[400, 404])
+
+            config_body = {}
+            es.indices.create(index=TEST_INDEX_NAME, ignore=400, body=config_body)
+        else:
+            self._searcher = None
+
+    def tearDown(self):
+        if self._is_elastic:
+            es = Elasticsearch()
+            es.indices.delete(index=TEST_INDEX_NAME, ignore=[400, 404])
+        else:
+            self._searcher = None
+
+    def test_factory_creator(self):
+        self.assertTrue(isinstance(self.searcher, SearchEngine))
+
+    def test_find_all(self):
+        test_string = "A test string"
+        self.searcher.index("test_doc", {"name": test_string})
+
+        # search everything
+        response = self.searcher.search(None)
+        self.assertEqual(response["total"], 1)
+        results = response["results"]
+        self.assertEqual(results[0]["data"]["name"], test_string)
+
+        self.searcher.index("not_test_doc", {"value": test_string})
+
+        response = self.searcher.search(None)
+        self.assertEqual(response["total"], 2)
+        results = response["results"]
+        test_0 = results[0]["data"] if "name" in results[0]["data"] else results[1]["data"]
+        test_1 = results[1]["data"] if "name" in results[0]["data"] else results[0]["data"]
+        self.assertEqual(test_0["name"], test_string)
+        self.assertEqual(test_1["value"], test_string)
+
+    def test_find_doctype(self):
+        test_string = "A test string"
+        self.searcher.index("test_doc", {"name": test_string})
+
+        # search by doc_type
+        response = self.searcher.search(None, doc_type="test_doc")
+        self.assertEqual(response["total"], 1)
+
+        response = self.searcher.search(None, doc_type="not_test_doc")
+        self.assertEqual(response["total"], 0)
+
+        self.searcher.index("not_test_doc", {"value": test_string})
+
+        response = self.searcher.search(None, doc_type="not_test_doc")
+        self.assertEqual(response["total"], 1)
+
+    def test_find_string(self):
+        test_string = "A test string"
+        self.searcher.index("test_doc", {"name": test_string})
+
+        # search string
+        response = self.searcher.search_string(test_string)
+        self.assertEqual(response["total"], 1)
+
+        self.searcher.index("not_test_doc", {"value": test_string})
+
+        response = self.searcher.search_string(test_string)
+        self.assertEqual(response["total"], 2)
+
+    def test_field(self):
+        test_string = "A test string"
+        test_object = {
+            "name": test_string,
+            "tags": {
+                "tag_one": "one",
+                "tag_two": "two"
+            },
+            "fieldX": "valueY"
+        }
+        self.searcher.index("test_doc", test_object)
+
+        # search tags
+        response = self.searcher.search_fields({"tags.tag_one": "one"})
+        self.assertEqual(response["total"], 1)
+
+        # search tags
+        response = self.searcher.search_fields({"tags.tag_one": "one", "tags.tag_two": "two"})
+        self.assertEqual(response["total"], 1)
+
+        response = self.searcher.search_fields({"fieldX": "valueY"})
+        self.assertEqual(response["total"], 1)
+
+        # search tags
+        response = self.searcher.search_fields({"tags.tag_one": "one", "tags.tag_two": "not_two"})
+        self.assertEqual(response["total"], 0)
+
+    def test_search_string_and_field(self):
+        test_object = {
+            "name": "You may find me in a coffee shop",
+            "course_id": "A/B/C",
+            "abc": "xyz",
+        }
+        self.searcher.index("test_doc", test_object)
+
+        response = self.searcher.search(query_string="find me")
+        self.assertEqual(response["total"], 1)
+
+        response = self.searcher.search_fields({"course_id": "A/B/C"})
+        self.assertEqual(response["total"], 1)
+
+        response = self.searcher.search(query_string="find me", field_dictionary={"course_id": "X/Y/Z"})
+        self.assertEqual(response["total"], 0)
+
+        response = self.searcher.search(query_string="find me", field_dictionary={"course_id": "A/B/C"})
+        self.assertEqual(response["total"], 1)
+
+        response = self.searcher.search_string("find me", field_dictionary={"course_id": "A/B/C"})
+        self.assertEqual(response["total"], 1)
+
+        response = self.searcher.search_fields({"course_id": "A/B/C"}, query_string="find me")
+        self.assertEqual(response["total"], 1)
+
+    def test_search_tags(self):
+        test_object = {
+            "name": "John Lester",
+            "course_id": "A/B/C",
+            "abc": "xyz"
+        }
+        tags = {
+            "color": "red",
+            "shape": "square",
+            "taste": "sour",
+        }
+        self.searcher.index("test_doc", test_object, tags=tags)
+
+        response = self.searcher.search_tags({"color": "red"})
+        self.assertEqual(response["total"], 1)
+        result = response["results"][0]
+        self.assertEqual(result["tags"]["color"], "red")
+        self.assertEqual(result["tags"]["shape"], "square")
+        self.assertEqual(result["tags"]["taste"], "sour")
+
+        response = self.searcher.search(tag_dictionary={"color": "red"})
+        self.assertEqual(response["total"], 1)
+        result = response["results"][0]
+        self.assertEqual(result["tags"]["color"], "red")
+        self.assertEqual(result["tags"]["shape"], "square")
+        self.assertEqual(result["tags"]["taste"], "sour")
+
+        response = self.searcher.search(tag_dictionary={"color": "blue"})
+        self.assertEqual(response["total"], 0)
+
+        response = self.searcher.search(tag_dictionary={"shape": "square"})
+        self.assertEqual(response["total"], 1)
+        result = response["results"][0]
+        self.assertEqual(result["tags"]["color"], "red")
+        self.assertEqual(result["tags"]["shape"], "square")
+        self.assertEqual(result["tags"]["taste"], "sour")
+
+        response = self.searcher.search(tag_dictionary={"shape": "round"})
+        self.assertEqual(response["total"], 0)
+
+        response = self.searcher.search(tag_dictionary={"shape": "square", "color": "red"})
+        self.assertEqual(response["total"], 1)
+        result = response["results"][0]
+        self.assertEqual(result["tags"]["color"], "red")
+        self.assertEqual(result["tags"]["shape"], "square")
+        self.assertEqual(result["tags"]["taste"], "sour")
+
+        response = self._searcher.search(tag_dictionary={"shape": "square", "color": "blue"})
+        self.assertEqual(response["total"], 0)
+
+class SearchResultProcessorTests(TestCase):
+
+    def test_strings_in_dictionary(self):
+        test_dict = {
+            "a": "This is a string that should show up"
+        }
+
+        get_strings = SearchResultProcessor.strings_in_dictionary(test_dict)
+        self.assertEqual(len(get_strings), 1)
+        self.assertEqual(get_strings[0], test_dict["a"])
+
+        test_dict.update({
+            "b": "This is another string that should show up"
+        })
+        get_strings = SearchResultProcessor.strings_in_dictionary(test_dict)
+        self.assertEqual(len(get_strings), 2)
+        self.assertEqual(get_strings[0], test_dict["a"])
+        self.assertEqual(get_strings[1], test_dict["b"])
+
+        test_dict.update({
+            "CASCADE": {
+                "z": "This one should be found too"
+            }
+        })
+        get_strings = SearchResultProcessor.strings_in_dictionary(test_dict)
+        self.assertEqual(len(get_strings), 3)
+        self.assertEqual(get_strings[0], test_dict["a"])
+        self.assertEqual(get_strings[1], test_dict["b"])
+        self.assertEqual(get_strings[2], test_dict["CASCADE"]["z"])
+
+        test_dict.update({
+            "DEEP": {
+                "DEEPER": {
+                    "STILL_GOING": {
+                        "MORE": {
+                            "here": "And here, again and again"
+                        }
+                    }
+                }
+            }
+        })
+        get_strings = SearchResultProcessor.strings_in_dictionary(test_dict)
+        self.assertEqual(len(get_strings), 4)
+        self.assertEqual(get_strings[0], test_dict["a"])
+        self.assertEqual(get_strings[1], test_dict["b"])
+        self.assertEqual(get_strings[2], test_dict["CASCADE"]["z"])
+        self.assertEqual(get_strings[3], test_dict["DEEP"]["DEEPER"]["STILL_GOING"]["MORE"]["here"])
+
+        test_dict = {
+            "id": "excluded",
+            "content_type": "excluded",
+            "xblock_keywords": "excluded",
+            "name": "not excluded",
+            "url": "excluded",
+        }
+
+        excluded_fields = ["id", "content_type", "xblock_keywords", "url"]
+        get_strings = SearchResultProcessor.strings_in_dictionary(test_dict, excluded_fields)
+        self.assertEqual(len(get_strings), 1)
+
+
+    def test_find_matches(self):
+        words = ["hello"]
+        strings = [
+            "hello there",
+            "goodbye",
+            "Sail away to say HELLO",
+        ]
+        matches = SearchResultProcessor.find_matches(strings, words, 100)
+        self.assertEqual(len(matches), 2)
+        self.assertEqual(matches[0], strings[0])
+        self.assertEqual(matches[1], strings[2])
+
+        words = ["hello", "there"]
+        strings = [
+            "hello there",
+            "goodbye",
+            "Sail away to say HELLO",
+        ]
+        matches = SearchResultProcessor.find_matches(strings, words, 100)
+        self.assertEqual(len(matches), 2)
+        self.assertEqual(matches[0], strings[0])
+        self.assertEqual(matches[1], strings[2])
+
+        words = ["hello", "there"]
+        strings = [
+            "hello there",
+            "goodbye there",
+            "Sail away to say HELLO",
+        ]
+        matches = SearchResultProcessor.find_matches(strings, words, 100)
+        self.assertEqual(len(matches), 3)
+        self.assertEqual(matches[0], strings[0])
+        self.assertEqual(matches[1], strings[2])
+        self.assertEqual(matches[2], strings[1])
+
+        words = ["goodbye there", "goodbye", "there"]
+        strings = [
+            "goodbye",
+            "goodbye there",
+            "Sail away to say GOODBYE",
+        ]
+        matches = SearchResultProcessor.find_matches(strings, words, 100)
+        self.assertEqual(len(matches), 3)
+        self.assertEqual(matches[0], strings[1])
+        self.assertEqual(matches[1], strings[0])
+        self.assertEqual(matches[2], strings[2])
+
+    def test_too_long_find_matches(self):
+        words = ["edx", "afterward"]
+        strings = [
+            "Here is a note about edx and it is very long - more than the desirable length of 100 characters - indeed this should show up",
+            "This matches too but comes afterward",
+        ]
+        matches = SearchResultProcessor.find_matches(strings, words, 100)
+        self.assertEqual(len(matches), 1)
+
+    def test_url(self):
+        test_result = {
+            "course": "testmetestme",
+            "id": "herestheid"
+        }
+        srp = SearchResultProcessor(test_result)
+        self.assertEqual(srp.url, "/courses/testmetestme/jump_to/herestheid")
+
+        srp = SearchResultProcessor({"course": "testmetestme"})
+        self.assertEqual(srp.url, None)
+
+        srp = SearchResultProcessor({"id": "herestheid"})
+        self.assertEqual(srp.url, None)
+
+        srp = SearchResultProcessor({"something_else": "altogether"})
+        self.assertEqual(srp.url, None)
+
+    def test_excerpt(self):
+        test_result = {
+            "notes": "Here is a note about edx",
+            "name": "edX search a lot",
+        }
+        srp = SearchResultProcessor(test_result)
+        edx_excerpt = srp.excerpt("note")
+        self.assertEqual(edx_excerpt, "Here is a <b>note</b> about edx")
+
+        edx_excerpt = srp.excerpt("edx")
+        self.assertEqual(edx_excerpt, "Here is a note about <b>edx</b>...<b>edX</b> search a lot")
+
+    def test_too_long_excerpt(self):
+        test_result = {
+            "notes": "Here is a note about edx and it is very long - more than the desirable length of 100 characters - indeed this should show up but it should trim the characters around in order to show the selected text in bold",
+        }
+        srp = SearchResultProcessor(test_result)
+        edx_excerpt = srp.excerpt("edx")
+        self.assertNotEqual(edx_excerpt, "Here is a note about <b>edx</b> and it is very long - more than the desirable length of 100 characters - indeed this should show up but it should trim the characters around in order to show the selected text in bold")
+        self.assertTrue("note about <b>edx</b> and it is" in edx_excerpt)
+
+        test_result = {
+            "notes": "Here is a note about stuff and it is very long - more than the desirable length of 100 characters - indeed this should show up but it should trim the edx characters around in order to show the selected text in bold",
+        }
+        srp = SearchResultProcessor(test_result)
+        edx_excerpt = srp.excerpt("edx")
+        self.assertNotEqual(edx_excerpt, "Here is a note about stuff and it is very long - more than the desirable length of 100 characters - indeed this should show up but it should trim the edx characters around in order to show the selected text in bold")
+        self.assertTrue("should trim the <b>edx</b> characters around" in edx_excerpt)
---
+++ search/views.py
@@ -0,0 +1,138 @@
+import json
+import re
+
+from django.http import HttpResponse
+from django.utils.translation import ugettext as _
+from django.views.decorators.csrf import csrf_exempt
+
+from manager import SearchEngine
+
+
+DESIRED_EXCERPT_LENGTH = 100
+ELLIPSIS = "&hellip;"
+EXCLUDE_FIELDS = ["id", "url", "content_type", "xblock_keywords", "course"]
+
+
+class SearchResultProcessor(object):
+
+    _results_fields = {}
+
+    def __init__(self, dictionary):
+        self._results_fields = dictionary
+
+    @staticmethod
+    def strings_in_dictionary(dictionary, excluded_fields = EXCLUDE_FIELDS):
+        local_dictionary = dict(dictionary)
+        for exclude_field in excluded_fields:
+            if exclude_field in local_dictionary:
+                del local_dictionary[exclude_field]
+        strings = [value for value in local_dictionary.itervalues() if not isinstance(value, dict)]
+        for child_dict in [dv for dv in local_dictionary.itervalues() if isinstance(dv, dict)]:
+            strings.extend(SearchResultProcessor.strings_in_dictionary(child_dict))
+        return strings
+
+    @staticmethod
+    def find_matches(strings, words, length_hoped):
+        matches = []
+        length_found = 0
+        for word in words:
+            length_found += sum([len(s) for s in strings if word.lower() in s.lower() and s not in matches])
+            matches.extend([s for s in strings if word.lower() in s.lower() and s not in matches])
+            if length_found >= length_hoped:
+                return matches
+        return matches
+
+    @staticmethod
+    def boldface_matches(match_in, match_word):
+        matches = re.finditer(match_word, match_in, re.IGNORECASE)
+        for matched_string in set([match.group() for match in matches]):
+            match_in = match_in.replace(matched_string, "<b>{}</b>".format(matched_string))
+        return match_in
+
+    def excerpt(self, match_phrase):
+
+        match_words = [match_phrase]
+        separate_words = match_phrase.split(' ')
+        if len(separate_words) > 1:
+            match_words.extend(match_phrase.split(' '))
+
+        matches = SearchResultProcessor.find_matches(
+            SearchResultProcessor.strings_in_dictionary(self._results_fields),
+            match_words,
+            DESIRED_EXCERPT_LENGTH
+        )
+        excerpt_text = '...'.join(matches)
+        if len(matches) == 1 and len(matches[0]) > DESIRED_EXCERPT_LENGTH:
+            # find first match and position
+            excerpt_text = matches[0]
+
+            word_at = -1
+            word_index = 0
+            while word_at < 0 and word_index < len(match_words):
+                word_at = excerpt_text.lower().find(match_words[word_index].lower())
+                word_index += 1
+
+            start_index = (word_at - DESIRED_EXCERPT_LENGTH / 2)
+            if start_index < 0:
+                start_index = 0
+            end_index = (word_at + DESIRED_EXCERPT_LENGTH / 2)
+            if end_index >= len(excerpt_text):
+                end_index = -1
+
+            excerpt_text = "{}{}{}".format(
+                "" if start_index < 1 else ELLIPSIS,
+                excerpt_text[start_index:end_index],
+                "" if end_index < 0 else ELLIPSIS,
+            )
+
+        for match_word in match_words:
+            excerpt_text = SearchResultProcessor.boldface_matches(excerpt_text, match_word)
+
+        return excerpt_text
+
+    @property
+    def url(self):
+        if "course" not in self._results_fields or "id" not in self._results_fields:
+            return None
+
+        return u"/courses/{course_id}/jump_to/{location}".format(
+            course_id=self._results_fields["course"],
+            location=self._results_fields["id"],
+        )
+
+
+@csrf_exempt
+def do_search(request, course_id=None):
+    results = {
+        "error": _("Nothing to search")
+    }
+    status_code = 500
+
+    try:
+        if request.method == 'POST':
+            search_terms = request.POST["search_string"]
+
+            field_dictionary = None
+            if course_id:
+                field_dictionary = {"course": course_id}
+            searcher = SearchEngine.get_search_engine("courseware_index")
+            results = searcher.search_string(
+                search_terms, field_dictionary=field_dictionary)
+
+            # update the data with the right url
+            for result_data in [result["data"] for result in results["results"]]:
+                result_data["excerpt"] = result_info.excerpt(search_terms)
+                result_info = SearchResultProcessor(result_data)
+                result_data["url"] = result_info.url
+
+            status_code = 200
+    except Exception as err:
+        results = {
+            "error": str(err)
+        }
+
+    return HttpResponse(
+        json.dumps(results),
+        content_type='application/json',
+        status=status_code
+    )
