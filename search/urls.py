1a6926de996755ae2402cd437089ce968a0aeb0b
Ahsan Ulhaq <ahsan.haq@arbisoft.com>
2017-07-10 15:55:53 +0500

    Upgrade to django 1.11 and implemented tox LEARNER-1529

--- search/urls.py
+++ search/urls.py
@@ -1,6 +1,6 @@
 """ expose courseware search http interface """
 from django.conf import settings
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from . import views
 
@@ -8,9 +8,8 @@ COURSE_ID_PATTERN = getattr(settings, "COURSE_ID_PATTERN", r'(?P<course_id>[^/+]
 
 # urlpatterns is the standard name to use here
 # pylint: disable=invalid-name
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^$', views.do_search, name='do_search'),
     url(r'^{}$'.format(COURSE_ID_PATTERN), views.do_search, name='do_search'),
     url(r'^course_discovery/$', views.course_discovery, name='course_discovery'),
-)
+]
