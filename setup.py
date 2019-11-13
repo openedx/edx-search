c49a0332733abdf8e60426629c8c7736e77f4cca
Jeremy Bowman <jbowman@edx.org>
2019-11-13 16:46:24 -0500

    Update tested Python versions

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
         'Framework :: Django',
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
