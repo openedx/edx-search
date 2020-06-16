5644b50aadedbdb3895b9ac08ac6046ec34b1d8d
Soban Javed <iamsobanjaved@gmail.com>
2020-06-16 20:26:36 +0500

    added python 3.8 travis worker
    increased coverage target

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def is_requirement(line):
 
 setup(
     name='edx-search',
-    version='1.4.0',
+    version='1.4.1',
     description='Search and index routines for index access',
     author='edX',
     author_email='oscm@edx.org',
