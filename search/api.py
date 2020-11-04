369b4fdba2381ce6bf1226506f0c2623f032922a
Omar Al-Ithawi <i@omardo.com>
2020-11-04 09:55:06 +0300

    python 3 and juniper updates

--- search/api.py
+++ search/api.py
@@ -84,11 +84,10 @@ def _hack_filter_discovery_results(results):
     This is a hack function that should be refactored into the LMS.
     See RED-637.
     """
-    from courseware.access import has_access
+    from lms.djangoapps.courseware.access import has_access
     from crum import get_current_request
     from opaque_keys.edx.keys import CourseKey
     from xmodule.modulestore.django import modulestore
-    import six
 
     ms = modulestore()
     user = get_current_request().user
@@ -110,12 +109,12 @@ def _hack_filter_discovery_results(results):
     # This is not the smartest hack, and customers could report issues
     # The solution is most likely to just remove the facet numbers
     results["total"] = max(0, results["total"] - access_denied_count)
-    for name, facet in six.iteritems(results["facets"]):
+    for name, facet in list(results["facets"].items()):
         results["total"] = max(0, results["total"] - access_denied_count)
         facet["other"] = max(0, facet.get("other", 0) - access_denied_count)
         facet["terms"] = {
             term: max(0, count - access_denied_count)
-            for term, count in six.iteritems(facet["terms"])
+            for term, count in list(facet["terms"].items())
             # Remove the facet terms that has no results
             if max(0, count - access_denied_count)
         }
