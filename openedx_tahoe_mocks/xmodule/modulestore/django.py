75332d32b429c45b7b5e6a346cb1807e3aba6607
Omar Al-Ithawi <i@omardo.com>
2020-11-05 14:23:40 +0300

    fix modulestore mocks issue

--- openedx_tahoe_mocks/xmodule/modulestore/django.py
+++ openedx_tahoe_mocks/xmodule/modulestore/django.py
@@ -3,6 +3,7 @@ Mocks for xmodule.modulestore.django tests can run.
 """
 
 from unittest.mock import Mock
+from opaque_keys.edx.keys import CourseKey
 
 
 def get_course(course_key, depth=0):
@@ -10,7 +11,7 @@ def get_course(course_key, depth=0):
     Mock get_course function.
     """
     assert depth == 0, 'Avoid loading entire course.'
-    assert str(course_key).startswith('course-v1:')
+    assert isinstance(course_key, CourseKey), 'this function expects a valid course key'
     course = Mock()
     course.id = course_key
     return course
