1a89d692c5e7f45780b6200c8002cd3dd4d8fffd
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-01-19 20:50:50 +0500

    Fixing url.

--- search/urls.py
+++ search/urls.py
@@ -11,6 +11,6 @@ COURSE_ID_PATTERN = getattr(settings, "COURSE_ID_PATTERN", r'(?P<course_id>[^/+]
 # pylint: disable=invalid-name
 urlpatterns = [
     url(r'^$', views.do_search, name='do_search'),
-    url(fr'^{COURSE_ID_PATTERN}$', views.do_search, name='do_search'),
+    url(r'^{}$'.format(COURSE_ID_PATTERN), views.do_search, name='do_search'),
     url(r'^course_discovery/$', views.course_discovery, name='course_discovery'),
 ]
