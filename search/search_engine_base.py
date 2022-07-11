6249ef59f95bf506cfe05dc7bc60859ce850463b
rayzhou-bit <rzhou@2u.com>
2022-07-11 05:34:54 -0400

    feat: create waffle switch force_elastic_search

--- search/search_engine_base.py
+++ search/search_engine_base.py
@@ -1,10 +1,21 @@
 """ Abstract SearchEngine with factory method """
 # This will get called by tests, but pylint thinks that it is not used
 
+from edx_toggles.toggles import WaffleSwitch
 from django.conf import settings
 
 from .utils import _load_class
 
+# .. toggle_name: force_elastic_search
+# .. toggle_implementation: WaffleSwitch
+# .. toggle_default: False
+# .. toggle_description: This flag forces the use of ElasticSearch. It prevents errors from switching to OpenSearch before roll out.
+# .. toggle_use_cases: temporary
+# .. toggle_creation_date: 2022-7-11
+# .. toggle_target_removal_date: None
+# .. toggle_tickets: TNL-9899
+# .. toggle_warnings: This temporary feature toggle does not have a target removal date.
+FORCE_ELASTIC_SEARCH = WaffleSwitch('force_elastic_search', __name__)
 
 class SearchEngine:
     """
@@ -58,5 +69,7 @@ class SearchEngine:
         """
         Returns the desired implementor (defined in settings).
         """
+        # TNL-9899
+        return _load_class("search.elastic.ElasticSearchEngine", None) if FORCE_ELASTIC_SEARCH
         search_engine_class = _load_class(getattr(settings, "SEARCH_ENGINE", None), None)
         return search_engine_class(index=index) if search_engine_class else None
