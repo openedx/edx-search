b73d424bc48ae771878c350a13a242544f81ea02
rayzhou-bit <rzhou@2u.com>
2022-07-15 01:23:29 -0400

    feat: test wip try_except

--- search/search_engine_base.py
+++ search/search_engine_base.py
@@ -71,12 +71,13 @@ class SearchEngine:
         """
         Returns the desired implementor (defined in settings).
         """
-        # TNL-9899
-        if DEFAULT_ELASTIC_SEARCH_SWITCH.is_enabled():
-            search_engine_class = _load_class("search.elastic.ElasticSearchEngine", None)
-            return search_engine_class(index=index)
-        else:
+        try:
             search_engine_class = _load_class(getattr(settings, "SEARCH_ENGINE", None), None)
             if search_engine_class:
+                return search_engine_class(index=index)
+        # TNL-9899
+        except:
+            if DEFAULT_ELASTIC_SEARCH_SWITCH.is_enabled():
+                search_engine_class = _load_class("search.elastic.ElasticSearchEngine", None)
                 return search_engine_class(index=index)
         return None
