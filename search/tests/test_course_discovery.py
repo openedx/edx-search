daa6fead8ff0c8dc9921daf11d35c6416de8e224
Serhii Nanai <serhii.nanai@raccoongang.com>
2025-07-09 16:50:35 +0300

    test: add integration tests

--- search/tests/test_course_discovery.py
+++ search/tests/test_course_discovery.py
@@ -4,8 +4,10 @@
 """ Tests for search functionalty """
 
 import copy
+import time
 from datetime import datetime
 import ddt
+import meilisearch
 
 from django.core.cache import cache
 from django.test import TestCase
@@ -16,6 +18,7 @@ from search.api import course_discovery_search, NoSearchEngineError
 from search.elastic import ElasticSearchEngine
 from search.tests.utils import SearcherMixin, TEST_INDEX_NAME
 from .mock_search_engine import MockSearchEngine
+from search.meilisearch import get_meilisearch_client, create_indexes
 
 
 class DemoCourse:
@@ -372,3 +375,288 @@ class TestNone(TestCase):
         """ search opertaion should yeild an exception with no search engine """
         with self.assertRaises(NoSearchEngineError):
             course_discovery_search("abc test")
+
+
+@override_settings(SEARCH_ENGINE="search.meilisearch.MeilisearchEngine")
+@override_settings(COURSEWARE_INFO_INDEX_NAME=TEST_INDEX_NAME)
+class TestMeilisearchCourseDiscoverySearch(TestCase, SearcherMixin):
+    """
+    Integration tests using real Meilisearch engine.
+    """
+
+    def setUp(self):
+        super().setUp()
+        create_indexes({TEST_INDEX_NAME: [
+            "language",
+            "modes",
+            "org",
+            "catalog_visibility",
+            "enrollment_start",
+            "enrollment_end",
+        ]})
+        self.wait_for_meilisearch_indexing()
+
+    def tearDown(self):
+        client = get_meilisearch_client()
+        try:
+            client.index(TEST_INDEX_NAME).delete()
+        except meilisearch.errors.MeilisearchApiError:
+            pass
+        super().tearDown()
+
+    @staticmethod
+    def wait_for_meilisearch_indexing():
+        from search.meilisearch import get_meilisearch_client
+        client = get_meilisearch_client()
+        task = client.index(TEST_INDEX_NAME).get_tasks().results[-1]
+        if not task:
+            return
+        client.wait_for_task(task.uid)
+        time.sleep(0.2)
+
+    def test_course_matching_empty_index(self):
+        """ Check for empty result count before indexing """
+        results = course_discovery_search("defensive")
+        self.assertEqual(results["total"], 0)
+
+    def test_course_matching(self):
+        """ Make sure that matches within content can be located and processed """
+        DemoCourse.get_and_index(self.searcher, {
+            "content": {
+                "short_description": "This is a defensive move",
+                "overview": "Defensive teams often win"
+            }
+        })
+        self.wait_for_meilisearch_indexing()
+
+        DemoCourse.get_and_index(self.searcher, {
+            "content": {
+                "short_description": "This is an offensive move",
+                "overview": "Offensive teams often win"
+            }
+        })
+        self.wait_for_meilisearch_indexing()
+
+        DemoCourse.get_and_index(self.searcher, {
+            "content": {
+                "short_description": "This is a hyphenated move",
+                "overview": "Highly-offensive teams often win"
+            }
+        })
+        self.wait_for_meilisearch_indexing()
+
+        results = course_discovery_search()
+        self.assertEqual(results["total"], 3)
+
+    @override_settings(COURSE_DISCOVERY_AGGREGATIONS={"subject": {}, "lang": {}})
+    def test_aggregating_override(self):
+        """
+        Test that aggregation under consideration can be specified
+        with custom setting
+        """
+        create_indexes({TEST_INDEX_NAME: [
+            "lang",
+            "subject",
+        ]})
+
+        DemoCourse.get_and_index(self.searcher, {"subject": "Mathematics", "lang": ["en", "fr"]})
+        DemoCourse.get_and_index(self.searcher, {"subject": "Mathematics", "lang": ["en"]})
+        DemoCourse.get_and_index(self.searcher, {"subject": "History", "lang": ["en"]})
+        DemoCourse.get_and_index(self.searcher, {"subject": "History", "lang": ["fr"]})
+        DemoCourse.get_and_index(self.searcher, {"lang": ["de"]})
+        self.wait_for_meilisearch_indexing()
+
+        results = course_discovery_search()
+        self.assertEqual(results["total"], 5)
+        self.assertIn("aggs", results)
+
+        self.assertNotIn("org", results["aggs"])
+        self.assertNotIn("modes", results["aggs"])
+
+        self.assertIn("subject", results["aggs"])
+        self.assertEqual(results["aggs"]["subject"]["total"], 4)
+        self.assertEqual(results["aggs"]["subject"]["terms"]["Mathematics"], 2)
+        self.assertEqual(results["aggs"]["subject"]["terms"]["History"], 2)
+
+        self.assertIn("lang", results["aggs"])
+        self.assertEqual(results["aggs"]["lang"]["total"], 6)
+        self.assertEqual(results["aggs"]["lang"]["terms"]["en"], 3)
+        self.assertEqual(results["aggs"]["lang"]["terms"]["fr"], 2)
+        self.assertEqual(results["aggs"]["lang"]["terms"]["de"], 1)
+
+    def test_course_list(self):
+        """ No arguments to course_discovery_search should show all available courses"""
+        results = course_discovery_search()
+        self.assertEqual(results["total"], 0)
+
+        DemoCourse.get_and_index(self.searcher)
+        self.wait_for_meilisearch_indexing()
+        results = course_discovery_search()
+        self.assertEqual(results["total"], 1)
+
+    def test_discovery_field_matching(self):
+        """ Test that field specifications only show those results with the desired field values """
+        DemoCourse.get_and_index(self.searcher, {"org": "OrgA"})
+        DemoCourse.get_and_index(self.searcher, {"org": "OrgB"})
+        self.wait_for_meilisearch_indexing()
+
+        results = course_discovery_search()
+        self.assertEqual(results["total"], 2)
+
+        results = course_discovery_search(field_dictionary={"org": "OrgA"})
+        self.assertEqual(results["total"], 1)
+        self.assertEqual(results["results"][0]["data"]["org"], "OrgA")
+
+        results = course_discovery_search(field_dictionary={"org": "OrgB"})
+        self.assertEqual(results["total"], 1)
+        self.assertEqual(results["results"][0]["data"]["org"], "OrgB")
+
+    def test_multivalue_field_matching(self):
+        """
+        Test that field specifications only show those results with the desired
+        field values - even when there is an array of possible values
+        """
+        DemoCourse.get_and_index(self.searcher, {"modes": ["honor", "verified"]})
+        DemoCourse.get_and_index(self.searcher, {"modes": ["honor"]})
+        self.wait_for_meilisearch_indexing()
+
+        results = course_discovery_search()
+        self.assertEqual(results["total"], 2)
+
+        results = course_discovery_search(field_dictionary={"modes": "honor"})
+        self.assertEqual(results["total"], 2)
+
+        results = course_discovery_search(field_dictionary={"modes": "verified"})
+        self.assertEqual(results["total"], 1)
+
+    def test_enroll_date(self):
+        """
+        Test that we don't show any courses that have no published enrollment date, or an enrollment date in the future
+        """
+        # demo_course_1 should be found cos it has a date that is valid
+        DemoCourse.get_and_index(self.searcher, {"enrollment_start": datetime(2014, 1, 1)})
+
+        # demo_course_2 should not be found because it has enrollment_start date set explicitly to None
+        DemoCourse.get_and_index(self.searcher, {"enrollment_start": None})
+
+        # demo_course_3 should not be found because it has enrollment_start date in the future
+        DemoCourse.get_and_index(self.searcher, {"enrollment_start": datetime(2114, 1, 1)})
+
+        # demo_course_4 should not be found because it has no enrollment_start specification
+        DemoCourse.get_and_index(self.searcher, {}, ["enrollment_start"])
+
+        self.wait_for_meilisearch_indexing()
+
+        results = course_discovery_search()
+        self.assertEqual(results["total"], 1)
+
+        additional_course = DemoCourse.get()
+        DemoCourse.index(self.searcher, [additional_course])
+        self.wait_for_meilisearch_indexing()
+
+        results = course_discovery_search()
+        self.assertEqual(results["total"], 2)
+
+        # Mark the course as having ended enrollment
+        additional_course["enrollment_end"] = datetime(2015, 1, 1)
+        DemoCourse.index(self.searcher, [additional_course])
+        self.wait_for_meilisearch_indexing()
+
+        results = course_discovery_search()
+        self.assertEqual(results["total"], 1)
+
+    def test_aggregating(self):
+        DemoCourse.get_and_index(self.searcher, {"language": "en", "org": "EDX"})
+        self.wait_for_meilisearch_indexing()
+
+        DemoCourse.get_and_index(self.searcher, {"language": "fr", "org": "ORG2"})
+        self.wait_for_meilisearch_indexing()
+
+        results = course_discovery_search(
+            search_term="",
+            field_dictionary={"language": ["en"]},
+            is_multivalue=True
+        )
+        self.assertDictEqual(results["aggs"]["language"]["terms"], {"en": 1, "fr": 1})
+        self.assertDictEqual(results["aggs"]["org"]["terms"], {"EDX": 1})
+
+    def test_aggregating_with_single_values_in_two_facets(self):
+        DemoCourse.get_and_index(self.searcher, {
+            "language": "en",
+            "org": "EDX",
+            "modes": "audit",
+        })
+        self.wait_for_meilisearch_indexing()
+
+        DemoCourse.get_and_index(self.searcher, {
+            "language": "en",
+            "org": "ORG2",
+            "modes": "honor",
+        })
+        self.wait_for_meilisearch_indexing()
+
+        results = course_discovery_search(
+            search_term="",
+            field_dictionary={"language": "en", "org": "EDX"}
+        )
+
+        self.assertIn("audit", results["aggs"]["modes"]["terms"])
+        self.assertNotIn("honor", results["aggs"]["modes"]["terms"])
+        self.assertEqual(results["aggs"]["language"]["terms"], {"en": 1})
+        self.assertEqual(results["aggs"]["org"]["terms"], {"EDX": 1})
+
+    def test_aggregating_with_multi_value_facet(self):
+        DemoCourse.get_and_index(self.searcher, {
+            "org": "EDX",
+            "language": "en",
+            "modes": "audit",
+        })
+        self.wait_for_meilisearch_indexing()
+
+        DemoCourse.get_and_index(self.searcher, {
+            "org": "EDX",
+            "language": "fr",
+            "modes": "honor",
+        })
+        self.wait_for_meilisearch_indexing()
+
+        DemoCourse.get_and_index(self.searcher, {
+            "org": "ORG2",
+            "language": "uk",
+            "modes": "verified",
+        })
+        self.wait_for_meilisearch_indexing()
+
+        results = course_discovery_search(
+            search_term="",
+            field_dictionary={"language": ["en", "fr"]},
+            is_multivalue=True
+        )
+
+        aggregations = results["aggs"]
+        self.assertIn("en", aggregations["language"]["terms"])
+        self.assertIn("fr", aggregations["language"]["terms"])
+        self.assertIn("uk", aggregations["language"]["terms"])
+        self.assertDictEqual(aggregations["language"]["terms"], {"en": 1, "fr": 1, "uk": 1})
+
+        self.assertNotIn("verified", aggregations["modes"]["terms"])
+        self.assertDictEqual(aggregations["modes"]["terms"], {"audit": 1, "honor": 1})
+
+        self.assertNotIn("ORG2", aggregations["org"]["terms"])
+        self.assertDictEqual(aggregations["org"]["terms"], {"EDX": 2})
+
+    def test_aggregating_facet_narrowed_if_single_value_search(self):
+        DemoCourse.get_and_index(self.searcher, {"language": "en", "modes": "audit"})
+        self.wait_for_meilisearch_indexing()
+
+        DemoCourse.get_and_index(self.searcher, {"language": "en", "modes": "honor"})
+        self.wait_for_meilisearch_indexing()
+
+        results = course_discovery_search(
+            search_term="",
+            field_dictionary={"modes": ["honor"]},
+            is_multivalue=False
+        )
+
+        self.assertNotIn("audit", results["aggs"]["modes"]["terms"])
+        self.assertDictEqual(results["aggs"]["modes"]["terms"], {'honor': 1})
