751451179f9c8468535de08eaf9100b817dce595
Serhii Nanai <serhii.nanai@raccoongang.com>
2025-07-29 21:45:37 +0300

    test: use manage.py test instead of pytest, remove conftest.py

--- conftest.py
+++
@@ -1,8 +0,0 @@
-import os
-import django
-
-# Set the Django settings module for test environment
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edxsearch.settings')
-
-# Setup Django
-django.setup()
