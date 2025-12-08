dbbbf9ec9abb90b2fdfe4cb1a3bbcc3653c7065b
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2025-12-08 15:28:10 +0200

    refactor: added ddt

--- search/tests/test_utils.py
+++ search/tests/test_utils.py
@@ -1,51 +1,46 @@
-"""Tests for utility functions"""
+"""Tests for utility functions in search.utils module."""
+
 import unittest
+from ddt import ddt, data, unpack
 
 from search.utils import normalize_bool
 
 
+@ddt
 class TestNormalizeBool(unittest.TestCase):
-    """Test cases for normalize_bool"""
+    """Test cases for normalize_bool function."""
 
-    def test_boolean_values(self):
-        cases = [
-            (True, True),
-            (False, False),
-        ]
-        for value, expected in cases:
-            with self.subTest(value=value):
-                assert normalize_bool(value) is expected
+    @data(
+        (True, True),
+        (False, False),
+    )
+    @unpack
+    def test_boolean_values(self, value, expected):
+        assert normalize_bool(value) is expected
 
-    def test_string_truthy_values(self):
-        truthy_strings = ['y', 'Y', 'yes', 'YES', 't', 'T', 'true', 'TRUE', 'on', 'ON', '1']
-        for value in truthy_strings:
-            with self.subTest(value=value):
-                assert normalize_bool(value) is True
+    @data('y', 'Y', 'yes', 'YES', 't', 'T', 'true', 'TRUE', 'on', 'ON', '1')
+    def test_string_truthy_values(self, value):
+        assert normalize_bool(value) is True
 
-    def test_string_falsy_values(self):
-        falsy_strings = ['n', 'N', 'no', 'NO', 'f', 'F', 'false', 'FALSE', 'off', 'OFF', '0']
-        for value in falsy_strings:
-            with self.subTest(value=value):
-                assert normalize_bool(value) is False
+    @data('n', 'N', 'no', 'NO', 'f', 'F', 'false', 'FALSE', 'off', 'OFF', '0')
+    def test_string_falsy_values(self, value):
+        assert normalize_bool(value) is False
 
-    def test_invalid_string_values(self):
-        invalid_values = ['invalid', '10']
-        for value in invalid_values:
-            with self.subTest(value=value):
-                with self.assertRaises(ValueError):
-                    normalize_bool(value)
+    @data('invalid', '10')
+    def test_invalid_string_values(self, value):
+        with self.assertRaises(ValueError):
+            normalize_bool(value)
 
-    def test_other_types(self):
-        cases = [
-            (1, True),
-            (0, False),
-            (100, True),
-            ([], False),
-            ([1, 2, 3], True),
-            ({}, False),
-            ({'key': 'value'}, True),
-            (None, False),
-        ]
-        for value, expected in cases:
-            with self.subTest(value=value):
-                assert normalize_bool(value) is expected
+    @data(
+        (1, True),
+        (0, False),
+        (100, True),
+        ([], False),
+        ([1, 2, 3], True),
+        ({}, False),
+        ({'key': 'value'}, True),
+        (None, False),
+    )
+    @unpack
+    def test_other_types(self, value, expected):
+        assert normalize_bool(value) is expected
